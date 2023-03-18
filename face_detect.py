1937185c2dd2bd3de5c1ac85b9ab9b7b347dcb51
christian bianchi <christian.bianchi23@gmail.com>
2023-03-18 11:49:08 +0100

    basic face detection with haarcascade

---
+++ face_detect.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+img = cv.imread('data/album_cropped.jpg')
+cv.imshow('Album', img)
+
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+# print(f'Number of faces found = {len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x + w, y + h), (0,255,0), thickness=2)
+
+cv.imshow('Detected Facess', img)
+
+cv.waitKey(0)
